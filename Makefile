37cbdcbeabfd71969cf40b4b4e7d57ec78a8ff23
Antonino Natale <antonio.natale97@hotmail.com>
2024-10-02 23:47:44 +0200

    feat: add aplus os implementation

---
+++ Makefile
@@ -0,0 +1,21 @@
+.PHONY: all clean
+
+CC ?= cc
+LD ?= cc
+
+OUTPUT=nesemu
+
+SRCS=$(shell find nes -name '*.c')
+HDRS=$(shell find nes -name '*.h')
+
+
+all: $(OUTPUT)
+
+$(OUTPUT): main.c $(SRCS) $(HDRS)
+	$(CC) -o $(OUTPUT) main.c $(SRCS) -I aplus/include -Wl,--gc-sections -D_DEFAULT_SOURCE
+
+install: $(OUTPUT)
+	mkdir -p $(DESTDIR)/usr
+	mkdir -p $(DESTDIR)/usr/bin
+	mkdir -p $(DESTDIR)/.pkg
+	install -m 0755 $(OUTPUT) $(DESTDIR)/usr/bin/$(OUTPUT)
---
+++ ci/prepare-and-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "### Prepare environemnt"
+
+mkdir -p sdk
+mkdir -p dist
+
+pushd sdk
+wget https://github.com/kwrx/aplus-toolchain/releases/latest/download/x86_64-aplus-toolchain.tar.xz
+tar xJf x86_64-aplus-toolchain.tar.xz
+popd
+
+git clone https://www.github.com/kwrx/aplus --depth=1
+
+export PATH=$(pwd)/sdk/bin:$PATH
+
+
+echo "### Build"
+
+make CC=x86_64-aplus-gcc DESTDIR=$(pwd)/dist -j$(nproc) install
+
+
+echo "### Package"
+
+tar -cJf nesemu.tar.xz -C dist .
